4f7c3889d36d6411a0b8365aeeaff1adc252827c
zhuqinfei <1586859762@qq.com>
2023-02-10 02:05:28 +0800

    获取请求消息体

--- index.ts
+++ index.ts
@@ -1,10 +1,22 @@
 import * as http from 'http';
+import {IncomingMessage, ServerResponse} from 'http';
 
 const server = http.createServer();
-
-server.on('request',(request,response)=>{
-  console.log('有人请求了')
-  response.end('hi')
+server.on('request',(request:IncomingMessage,response:ServerResponse)=>{
+  console.log(request.method)
+  console.log(request.url)
+  console.log(request.headers)
+  const array:any=[]
+  request.on('data',(chunk)=>{
+     array.push(chunk)
+  })
+  request.on('end',()=>{
+    const body=Buffer.concat(array).toString()
+    console.log('body')
+    console.log(body)
+    response.statusCode=404
+    response.end('hi')
+  })
 })
 
 server.listen(8888)
