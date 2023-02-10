f282baed94e07a294d56dde75c532722eb452535
zhuqinfei <1586859762@qq.com>
2023-02-11 02:25:53 +0800

    根据URL完成不同文件响应

--- index.ts
+++ index.ts
@@ -1,22 +1,38 @@
 import * as http from 'http';
 import {IncomingMessage, ServerResponse} from 'http';
+import * as fs from 'fs';
+import * as p from 'path';
+
 
 const server = http.createServer();
+const publicDir=p.resolve(__dirname,'public');
+
 server.on('request',(request:IncomingMessage,response:ServerResponse)=>{
-  console.log(request.method)
-  console.log(request.url)
-  console.log(request.headers)
-  const array:any=[]
-  request.on('data',(chunk)=>{
-     array.push(chunk)
-  })
-  request.on('end',()=>{
-    const body=Buffer.concat(array).toString()
-    console.log('body')
-    console.log(body)
-    response.statusCode=404
-    response.end('hi')
-  })
+  const {method,url,headers}=request
+  switch (url) {
+    case '/index.html':
+      response.setHeader('Content-Type','text/html;charset-utf-8')
+      fs.readFile(p.resolve(publicDir,'index.html'),(error,data)=>{
+        if(error) throw error;
+        response.end(data.toString());
+      });
+      break;
+    case '/style.css':
+      response.setHeader('Content-Type','text/css;charset-utf-8')
+      fs.readFile(p.resolve(publicDir,'style.css'),(error,data)=>{
+        if(error) throw error;
+        response.end(data.toString());
+      });
+      break;
+    case '/main.js':
+      response.setHeader('Content-Type','text/javascript;charset-utf-8')
+      fs.readFile(p.resolve(publicDir,'main.js'),(error,data)=>{
+        if(error) throw error;
+        response.end(data.toString());
+      });
+      break;
+  }
+  // response.end('hi')
 })
 
 server.listen(8888)
