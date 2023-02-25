1ed6d475917acbb47b3c299d0a3d4b3fafcbe6bb
zhuqinfei <1586859762@qq.com>
2023-02-25 14:57:07 +0800

    匹配任意文件和处理不存在文件

--- index.ts
+++ index.ts
@@ -11,32 +11,32 @@ server.on('request',(request:IncomingMessage,response:ServerResponse)=>{
   const {method,url:path,headers}=request
   // @ts-ignore
   const {pathname,search}=url.parse(path)
-  switch (pathname) {
-    case '/index.html':
-      response.setHeader('Content-Type','text/html;charset-utf-8')
-      fs.readFile(p.resolve(publicDir,'index.html'),(error,data)=>{
-        if(error) throw error;
-        response.end(data.toString());
-      });
-      break;
-    case '/style.css':
-      response.setHeader('Content-Type','text/css;charset-utf-8')
-      fs.readFile(p.resolve(publicDir,'style.css'),(error,data)=>{
-        if(error) throw error;
-        response.end(data.toString());
-      });
-      break;
-    case '/main.js':
-      response.setHeader('Content-Type','text/javascript;charset-utf-8')
-      fs.readFile(p.resolve(publicDir,'main.js'),(error,data)=>{
-        if(error) throw error;
-        response.end(data.toString());
-      });
-      break;
-    default:
-      response.statusCode=404;
-      response.end()
+  // @ts-ignore
+  let fileName=pathname.substr(1)
+  if(fileName===''){
+    fileName='index.html'
   }
+  fs.readFile(p.resolve(publicDir,fileName),(error,data)=>{
+    if(error){
+      if(error.errno===-4058){
+        response.statusCode=404
+        fs.readFile(p.resolve(publicDir,'404.html'),(error,data)=>{
+          response.end(data)
+        })
+      }else if(error.errno===-4068){
+        response.statusCode=403
+        response.setHeader('content-Type','text/html;charset=UTF-8')
+        response.end('无权查看目录内容')
+      }
+      else{
+        response.statusCode=505
+        response.setHeader('Content-Type','text/html;charset=UTF-8')
+        response.end('服务器繁忙，请稍后再试')
+      }
+    }else{
+      response.end(data);
+    }
+  })
 })
 
 server.listen(8888)
